235d5cca7027b25446a07d4aac897cd7c3a769c2
PrathyushaLakkireddy <prathyusha@vitwit.com>
2020-10-15 14:19:48 +0530

    Added instruction doc

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	client "github.com/influxdata/influxdb1-client/v2"
+
+	"github.com/Chainflow/oasis-mission-control/config"
+	"github.com/Chainflow/oasis-mission-control/targets"
+)
+
 func main() {
 
+	cfg, err := config.ReadFromFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := targets.InitTargets(cfg)
+	runner := targets.NewRunner()
+
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     fmt.Sprintf("http://localhost:%s", cfg.InfluxDB.Port),
+		Username: cfg.InfluxDB.Username,
+		Password: cfg.InfluxDB.Password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	var wg sync.WaitGroup
+	for _, tg := range m.List {
+		wg.Add(1)
+		go func(target targets.Target) {
+			scrapeRate, err := time.ParseDuration(target.ScraperRate)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for {
+				runner.Run(target.Func, target.HTTPOptions, cfg, c)
+				time.Sleep(scrapeRate)
+			}
+		}(tg)
+	}
+	wg.Wait()
 }
